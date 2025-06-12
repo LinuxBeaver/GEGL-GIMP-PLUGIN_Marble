2119df157c59ac608a051f317846181dcf411af2
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:49:05 -0400

    your message here

--- SourceCode/marble/marblewallpaper.c
+++ SourceCode/marble/marblewallpaper.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Credit to Øvind Kolas (pippin) for major GEGL contributions
  * 2023 - Marble Gimp Plugin by Beaver
 If you paste this syntax in Gimp's GEGL Graph filter you can test this filter without installing
 The syntax is also listed so users can study how this filter was made.
